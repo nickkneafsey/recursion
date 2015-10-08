ea9693849bfc04e149e17f9ff39c743fba5f6c8d
Nicholas Kneafsey <nicholaskneafsey@gmail.com>
2015-10-08 14:05:58 -0700

    added get elementsbyclassname function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+
+//You should use document.body, element.childNodes, and element.classList
+//document.body --Returns the <body> or <frameset> node of the current document, or null if no such element exists.
+//element.childNodes --The childNodes property returns an array of child nodes inside of a specified parent element.
+//element.classList --The Element.classList read-only property returns a live DOMTokenList of the class attributes of the element.
+
+
+
+var getElementsByClassName = function(className){
   // your code here
+  var elementArray=[];
+  var classSearch=function(element){
+  	var classes=element.classList;
+  	if (classes!==undefined){
+  		for (var i=0; i<classes.length; i++){
+  			if (classes[i]===className){
+  				elementArray.push(element);
+  			}
+  		}
+  	}
+  	var children = element.childNodes;
+  	if(children!==undefined){
+  		for (var j=0; j<children.length; j++){
+  			classSearch(children[j]);
+  		}
+  	}
+  }
+  classSearch(document.body);
+  return elementArray;
 };
